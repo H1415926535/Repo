29193458f170becb00192af1806fe07f06b64342
H1415926535 <karen_simonyan_2001@mail.ru>
2017-05-19 18:40:38 +0400

    server

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const http = require('http');
-
+const p = require('./ports');
 const server = http.createServer( (req,res) => {
   if (req.url === '/'){
     res.setHeader('content-type','text/html');
     res.end(`
       <!doctype html>
       <body>
-        <h1>Header </h1>
+        <h1> ${p.upper(__dirname)} </h1>
         <p> parag </p>
       </body>
       `)
